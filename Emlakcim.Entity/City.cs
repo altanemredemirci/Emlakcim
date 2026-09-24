4775eac9f0549b60854980f16a84e65689dbcb0a
altanemredemirci <altan_emre_1989@hotmail.com>
2026-09-24 21:12:09 +0300

    Entities Oluşturuldu

---
+++ Emlakcim.Entity/City.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Emlakcim.Entity
+{
+    public class City
+    {
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public List<District> Districts { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ Emlakcim.Entity/ProductType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Emlakcim.Entity
+{
+    public class ProductType
+    {
+        public int Id { get; set; }
+
+        [StringLength(30)]
+        public string Icon { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }
+        public bool Status { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
